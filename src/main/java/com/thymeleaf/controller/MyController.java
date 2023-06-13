4c1cf57f69f4c914bbc111a921787f99a1d037e7
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-13 16:01:57 +0500

    Passed the attribute through controller to about page and it show the attribute there

--- src/main/java/com/thymeleaf/controller/MyController.java
+++ src/main/java/com/thymeleaf/controller/MyController.java
@@ -1,13 +1,19 @@
 package com.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class MyController {
 
     @GetMapping("/about")
-    public String about(){
+    public String about(Model model){
+
+        model.addAttribute("name","Ali Raza");
+        model.addAttribute("currentDate", new Date());
 
         System.out.println("Inside about handler");
         return "about";
